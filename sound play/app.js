2adc1d48278672bcf02b65659d4106edc209b28f
Samin Yasar <67989825+saminyasar004@users.noreply.github.com>
2020-11-04 10:07:31 +0600

    write some comment

--- sound play/app.js
+++ sound play/app.js
@@ -1,13 +1,21 @@
+// to define all sounds name
 const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
 
+// sounds for each function
 sounds.forEach((sound) => {
+// create button for all sounds
   const btn = document.createElement("button");
+//   give a class name to button
   btn.classList.add("btn");
+//   button text
   btn.innerText = sound;
+//   append the button
   document.body.appendChild(btn);
   btn.addEventListener("click", () => {
+//     when click a once button pause all other sounds
     pauseSong();
     document.getElementById(sound).play();
+//     set the sound volume
     document.getElementById(sound).volume = 0.05;
   });
 });
