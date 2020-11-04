3a4b1904e1b3771a019821b8a0ac5e0b40dd07f5
Samin Yasar <yasarsamin57@gmail.com>
2020-11-04 10:03:17 +0600

    Finally Sound Play project is completed!

--- sound play/app.js
+++ sound play/app.js
@@ -1,1 +1,21 @@
 const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerText = sound;
+  document.body.appendChild(btn);
+  btn.addEventListener("click", () => {
+    pauseSong();
+    document.getElementById(sound).play();
+    document.getElementById(sound).volume = 0.05;
+  });
+});
+
+function pauseSong() {
+  sounds.forEach((sound) => {
+    const currentSound = document.getElementById(sound);
+    currentSound.pause();
+    currentSound.currentTime = 0;
+  });
+}
